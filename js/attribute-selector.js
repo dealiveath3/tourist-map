d97fb6396e7da23986d163bc7ebc3509a30f28ed
dealiveath3 <aldeathive@gmail.com>
2025-06-10 05:30:16 +0900

    Update attribute-selector.js

--- js/attribute-selector.js
+++ js/attribute-selector.js
@@ -2,9 +2,13 @@ let selectedAttribute = null;
 
 document.querySelectorAll('.attribute-button').forEach(button => {
   button.addEventListener('click', () => {
+    // すべてのボタンを解除
     document.querySelectorAll('.attribute-button').forEach(b => b.classList.remove('selected'));
-    selectedAttribute = button.textContent;
+    
+    // 自分を選択
     button.classList.add('selected');
+    selectedAttribute = button.textContent;
+
     console.log("選択中の属性:", selectedAttribute);
   });
 });

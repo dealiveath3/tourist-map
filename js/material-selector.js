29565409829655d05eecdd21f60bed59741d051c
dealiveath3 <aldeathive@gmail.com>
2025-06-10 05:36:02 +0900

    Update material-selector.js

--- js/material-selector.js
+++ js/material-selector.js
@@ -5,13 +5,15 @@ document.querySelectorAll('.material-button').forEach(button => {
     const material = button.textContent;
 
     if (selectedMaterials.includes(material)) {
+      // 解除
       selectedMaterials = selectedMaterials.filter(m => m !== material);
       button.classList.remove('selected');
     } else {
+      // 選択
       selectedMaterials.push(material);
       button.classList.add('selected');
     }
 
-    console.log("選択中の素材:", selectedMaterials);
+    console.log("選択された素材:", selectedMaterials);
   });
 });
